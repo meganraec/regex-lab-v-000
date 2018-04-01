66974ca659e0bc2aba7dc3eb5a027090c8d26fec
Megan Collier <meganraec@live.com>
2018-04-01 20:18:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,8 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^(?:\(?[A-Z])\)?\W$/)
-  # text.match(/^[A-Z]/) && text.match(/\W$/) ? true : false
+  text.match(/^[A-Z]/) && text.match(/\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
