beb25e1b9bd3d420549a80c16b760ba865e675b6
Megan Collier <meganraec@live.com>
2018-04-01 20:14:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^(?:\(?[A-Z])\)?(\W$)?/)
+  text.match(/^(?:\(?[A-Z])\)?(\W$)/)
   # text.match(/^[A-Z]/) && text.match(/\W$/) ? true : false
 end
 
