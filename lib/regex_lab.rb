55f79c029850b8659d1d083b2df28867b227db9f
Megan Collier <meganraec@live.com>
2018-04-01 20:12:06 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/)
+    return true
+  else false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]/) && text.match(/\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/^(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/)
 end
